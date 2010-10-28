57acd30d2da939f17b8d780004f0c7c0a2a9590a
Abhishek Mishra <ideamonk@gmail.com>
2010-10-28 23:38:15 +0530

    added a bit to init, feeling too sleepy to continue tonight

--- src/audio-hover.js
+++ src/audio-hover.js
@@ -6,8 +6,10 @@
 */
 
 function AudioHover(){
+    control_cache = {};
+    
     this.hover_in = function(){
-        console.log("I am in");
+        console.log($(this).attr('class'));
     };
     
     this.hover_out = function(){
@@ -15,8 +17,15 @@ function AudioHover(){
     };
     
     this.init = function(class_name) {
+        this.control_cache = {};
+        
         if (class_name == undefined)
             class_name = "audiohover";
+            
+        $("." + class_name).each(function(){
+            var new_audio = document.createElement("audio");
+            control_cache[$(this).attr('class')] = new_audio;
+        });
         
         $("." + class_name).hover(
             this.hover_in,
