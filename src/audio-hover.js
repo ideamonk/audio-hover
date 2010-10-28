45bf10714c4262b9d1ad988c0c71bb71d43ad46b
Abhishek Mishra <ideamonk@gmail.com>
2010-10-28 22:55:31 +0530

    done with basic hover setups

--- src/audio-hover.js
+++ src/audio-hover.js
@@ -1,5 +1,26 @@
 /*!
  * Audio Hover beta
+ * depends @jQuery
  *
  * Copyright 2010, Abhishek Mishra
 */
+
+function AudioHover(){
+    this.hover_in = function(){
+        console.log("I am in");
+    };
+    
+    this.hover_out = function(){
+        console.log("I am out");
+    };
+    
+    this.init = function(class_name) {
+        if (class_name == undefined)
+            class_name = "audiohover";
+        
+        $(".audiohover").hover(
+            this.hover_in,
+            this.hover_out
+        );
+    };
+}
